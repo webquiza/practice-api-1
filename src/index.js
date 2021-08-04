432bd0c1f4936ca7d6a6ddd0805e5887397b7932
Carlos Urquiza <webquiza@gmail.com>
2021-08-03 20:00:08 -0700

    separate promise logic

--- src/index.js
+++ src/index.js
@@ -2,35 +2,32 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import WeatherService from './weather-service';
+
+function clearFields() {
+  $('#zipCode').val("");
+  $('.showErrors').text("");
+  $('.showTemp').text("");
+}
 
 
 $('#weatherLocation').click(function() {
-  const zipCode = $('#zipCode').val();
-  $('#zipCode').val("");
+  let zipCode = $('#zipCode').val();
+  clearFields();
 
-  // Wrap XMLHttpRequest() in a promise.
-
-  let promise = new Promise(function(resolve, reject) {
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&units=imperial&appid=${process.env.API_KEY}`;
-    request.onload = function() {
-      if (this.status === 200) {
-        resolve(request.response); // Do this on resolution of promise.
-      } else {
-        reject(request.response); // Do this on rejection of promise.
-      }
-    };
-    request.open("GET", url, true);
-    request.send();
-  });
+  let promise = WeatherService.getWeather(zipCode);
 
   promise.then(function(response) {
+
     const body = JSON.parse(response);
     $('.showTemp').text(`The temperature in ${zipCode} is ${body.main.temp} °F`);
     $('.showErrors').text("");
+
   }, function(error) {
+
     $('.showErrors').text(`There was an error processing your request: ${error}`);
     $('.showTemp').text("");
+
   });
 });
   
